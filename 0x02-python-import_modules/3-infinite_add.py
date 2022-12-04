9fde33395f7a4a240ed4cebdb07827e1bd9b1488
NuelUzoma <emcool17@gmail.com>
2022-12-04 03:33:15 +0100

    Addition of passed Arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv as a
+    leg = len(a)
+    if leg == 1:
+        print('0')
+    elif leg == 2:
+        print(a[1])
+    elif leg == 3:
+        print(int(a[1]) + int(a[2]))
+    elif leg == 4:
+        print(int(a[1]) + int(a[2]) + int(a[3]))
+    elif leg == 5:
+        print(int(a[1]) + int(a[2]) + int(a[3]) + int(a[4]))
