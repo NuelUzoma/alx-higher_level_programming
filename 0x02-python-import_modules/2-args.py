f2b51068dee0d8824c6ae46eec784552632d6dac
NuelUzoma <emcool17@gmail.com>
2022-12-04 01:57:59 +0100

    Command Line Arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    leng = len(argv)
+    from sys import argv as a
+    leng = len(a)
     if leng == 1:
         print("0 arguments.")
-    if leng == 2:
-        print(f"1 argument:\n1: {argv[1]}")
-    if leng > 2:
-        print(f"{leng} arguments:\n1: {argv[1]}\n2: {argv[2]}\n3: {argv[3]}\n4: {argv[4]}\n5: {argv[5]}\n6: {argv[6]}\n7: {argv[7]}\n8: {argv[8]}")
+    elif leng == 2:
+        print(f"1 argument:\n1: {a[1]}")
+    elif leng == 3:
+        print(f"2 arguments:\n1: {a[1]}\n2: {a[2]}")
+    elif leng == 4:
+        print(f"3 arguments:\n1: {a[1]}\n2: {a[2]}\n3: {a[3]}")
+    elif leng == 5:
+        print(f"4 arguments:\n1:{a[1]}\n2:{a[2]}\n3:{a[3]}\n4:{a[4]}")
