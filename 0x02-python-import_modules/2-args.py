a21b4704d6017ff132da6d0d2021faee3ef48036
NuelUzoma <emcool17@gmail.com>
2022-12-04 01:28:30 +0100

    Arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-from sys import argv
-leng = len(argv)
-if leng == 1:
-    print("0 arguments.")
-if leng == 2:
-    print(f"1 argument:\n1: {argv[1]}")
-if leng > 2:
-    print(f"{leng} arguments:\n1: {argv[1]}\n2: {argv[2]}\n3: {argv[3]}\n4: {argv[4]}\n5: {argv[5]}\n6: {argv[6]}\n7: {argv[7]}\n8: {argv[8]}")
+if __name__ == "__main__":
+    from sys import argv
+    leng = len(argv)
+    if leng == 1:
+        print("0 arguments.")
+    if leng == 2:
+        print(f"1 argument:\n1: {argv[1]}")
+    if leng > 2:
+        print(f"{leng} arguments:\n1: {argv[1]}\n2: {argv[2]}\n3: {argv[3]}\n4: {argv[4]}\n5: {argv[5]}\n6: {argv[6]}\n7: {argv[7]}\n8: {argv[8]}")
