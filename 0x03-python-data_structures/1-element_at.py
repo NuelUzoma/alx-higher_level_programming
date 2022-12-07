33a7f603a4d8c25c24ae731ee3babe112cf17c2d
NuelUzoma <emcool17@gmail.com>
2022-12-07 02:15:17 +0100

    Correction of Task 0 and 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    idx = my_list[3]
-    if idx == None:
+    if idx < 0:
         return (None)
-    if idx > len(my_list):
+    if idx > len(my_list) - 1:
         return (None)
+    return(my_list[idx])
