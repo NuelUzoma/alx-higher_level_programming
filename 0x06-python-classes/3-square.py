22de05406afef3f90fd370db9e5ef5e90e4df6e1
NuelUzoma <emcool17@gmail.com>
2023-01-02 02:09:21 +0100

    a class Square that defines a square with a size

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square by: (based on 2-square.py)"""
+
+
+class Square:
+
+    def __init__(self, _Square__size=0):
+        """This contains the conditional statements"""
+        if not isinstance(_Square__size, int):
+            raise TypeError("size must be an integer")
+        elif (_Square__size < 0):
+            raise ValueError("size must be >= 0")
+        self._Square__size = _Square__size
+
+    def area(self):
+        """This contains the area of the square"""
+        return (self._Square__size ** 2)
