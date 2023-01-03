e3d8adea5fb810f402079b1ee90b085f38806532
NuelUzoma <emcool17@gmail.com>
2023-01-03 09:31:49 +0100

    Code Fix: Task 3 and Task 4

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,40 +1,44 @@
 #!/usr/bin/python3
-"""Write a class Rectangle that defines by: (based on 1-rectangle.py)"""
+"""Write a class Rectangle that defines by: (based on 3-rectangle.py)"""
 
 
 class Rectangle:
-    """The class Rectangle that defines a rectangle"""
+    """A class Rectangle that defines a rectangle"""
     def __init__(self, _Rectangle__width=0, _Rectangle__height=0):
-        """The initialization of the Rectangle"""
         self._Rectangle__height = _Rectangle__height
         self._Rectangle__width = _Rectangle__width
 
     def area(self):
-        """The area of the Rectangle"""
+        """This instance defines the area"""
         return self._Rectangle__height * self._Rectangle__width
 
     def perimeter(self):
-        """The perimeter of the Rectangle"""
+        """This instance defines the pereimeter"""
         if (self._Rectangle__height == 0 or self._Rectangle__width == 0):
             return (0)
         else:
-            return 2 * (self._Rectangle__height + self._Rectangle__width)
+            return 2 * (self._Rectangle__height + self.Rectangle__width)
 
     def __str__(self):
-        """The __str__ instance function"""
+        """The __str__ instance"""
         if (self._Rectangle__height == 0 or self._Rectangle__width == 0):
             return ''
         return '\n'.\
             join(['#' * self._Rectangle__width] * self._Rectangle__height)
 
+    def __repr__(self):
+        """The __repr__ instance to return a string representation"""
+        return f'Rectangle(_Rectangle__width={self._Rectangle__width},\
+        _Rectangle__height={self._Rectangle__height})'
+
     @property
     def height(self):
-        """The height instance getter"""
+        """The private instance attribute with getter"""
         return self._Rectangle__height
 
     @height.setter
     def height(self, value):
-        """The height instance setter"""
+        """The private instance attribute with setter"""
         self._Rectangle__height = value
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
@@ -43,12 +47,12 @@ class Rectangle:
 
     @property
     def width(self):
-        """The width instance getter"""
-        return self._Rectangle__height
+        """The private instance attribute with getter"""
+        return self._Rectangle__width
 
     @width.setter
     def width(self, value):
-        """The width instance setter"""
+        """The private instance attribute with setter"""
         self._Rectangle__width = value
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
