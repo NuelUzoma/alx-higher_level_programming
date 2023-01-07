70324cbc9472a2ec785dee7d2324239f9ec3454f
NuelUzoma <emcool17@gmail.com>
2023-01-07 04:48:39 +0100

    Task 4 for 0x07

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Write a function that prints a text
+        with 2 new lines after each of these characters: ., ? and :
+        text must be a string
+        otherwise raise a TypeError
+        There should be no space at each end of printed line"""
+
+
+def text_indentation(text):
+    """The text indentation function
+    text must be a string
+    No space allowed at beginning or end"""
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for i, c in enumerate(text):
+        if c == '.' or c == '?' or c == ':':
+            print(text[i+1:i+3])
+        else:
+            print(c, end='')
+        
