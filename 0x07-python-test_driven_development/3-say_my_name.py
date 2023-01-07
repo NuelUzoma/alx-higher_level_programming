3896746a908715eb4c412e6d5e4dc330d4a01a03
NuelUzoma <emcool17@gmail.com>
2023-01-07 03:41:12 +0100

    A function that prints the firstname and lastname

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Write a function that prints
+        a first name and a last name
+        firstname and lastname must be strings
+        otherwise raise a TyoeError
+        No module must be imported"""
+
+
+def say_my_name(first_name, last_name=""):
+    """The say_my_name function
+    containing two arguments
+    firstaname and lastname"""
+
+    print(f"My name is {first_name} {last_name}")
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    elif not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
