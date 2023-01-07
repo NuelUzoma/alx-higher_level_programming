bcf840b4954ede2eff189b705e656c68610b805e
NuelUzoma <emcool17@gmail.com>
2023-01-07 03:53:51 +0100

    Code Fix: Task 2

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -11,7 +11,6 @@ def say_my_name(first_name, last_name=""):
     containing two arguments
     firstaname and lastname"""
 
-
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     elif not isinstance(last_name, str):
