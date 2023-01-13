9a3527430f92af49b01a5fb949e9792d7ced202d
NuelUzoma <emcool17@gmail.com>
2023-01-13 10:08:57 +0100

    Code Fix: Task 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -21,3 +21,19 @@ class BaseGeometry:
         elif (value <= 0):
             raise ValueError(f"{name} must be greater than 0")
 
+class Rectangle(BaseGeometry):
+    """The class Rectangle which defines a Rectangle"""
+    def __init__(self, width, height):
+        """The initialization"""
+        self.__width = width
+        self.__height = height
+        super().integer_validator("width", self.__width)
+        super().integer_validator("height", self.__height)
+
+    def area(self):
+        """The implemented area of the Rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """The method to print"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
