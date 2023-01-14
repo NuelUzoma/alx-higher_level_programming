a608b732305a35b8d71ca9d7bb827a181e698e31
NuelUzoma <emcool17@gmail.com>
2023-01-14 12:01:37 +0100

    Almost a Circle: Task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Create a file named models/base.py:
+        Class Base:
+        private class attribute __nb_objects = 0
+        class constructor: def __init__(self, id=None)::
+        if id is not None, assign the public instance attribute id with this
+        argument value - you can assume id is an integer and
+        you don’t need to test the type of it
+        otherwise, increment __nb_objects and assign
+        the new value to the public instance attribute id"""
+
+
+class Base:
+    """The class Base with a private class attribute and class constructor"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """The class constructor containing an ID"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
