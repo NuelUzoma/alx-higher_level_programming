3e20b2a4d61d945873e5eb1a5aa9c93666390245
NuelUzoma <emcool17@gmail.com>
2023-01-15 03:51:55 +0100

    Almost a Circle: Task 9

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -49,18 +49,21 @@ class Rectangle(Base):
         for i in range(self.__y):
             print('#' * self.__x)
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """The method for assigning an argument to each attribute"""
         if len(args) > 0:
             self.id = args[0]
-        if len(args) > 1:
+        elif len(args) > 1:
             self.__width = args[1]
-        if len(args) > 2:
+        elif len(args) > 2:
             self.__height = args[2]
-        if len(args) > 3:
+        elif len(args) > 3:
             self.__x = args[3]
-        if len(args) > 4:
+        elif len(args) > 4:
             self.__y = args[4]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
     def __str__(self):
         """The method to print a string"""
@@ -106,7 +109,7 @@ class Rectangle(Base):
         self.__x = value
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
-        elif (x < 0):
+        elif (value < 0):
             raise ValueError("x must be >= 0")
 
     @property
@@ -120,5 +123,5 @@ class Rectangle(Base):
         self.__y = value
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
-        elif (y < 0):
+        elif (value < 0):
             raise ValueError("y must be >= 0")
