f7c3110f2b79c9a511b21a30c07034a0f79019cb
NuelUzoma <emcool17@gmail.com>
2023-01-15 03:35:12 +0100

    Almost a Circle: Task 5 to 8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -18,6 +18,7 @@
 class Base:
     """The parent class"""
     __nb_objects = 0
+
     def __init__(self, id=None):
         """The initialization"""
         if id is not None:
@@ -45,6 +46,26 @@ class Rectangle(Base):
         """The method to display in #"""
         for i in range(self.__height):
             print('#' * self.__width)
+        for i in range(self.__y):
+            print('#' * self.__x)
+
+    def update(self, *args):
+        """The method for assigning an argument to each attribute"""
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.__width = args[1]
+        if len(args) > 2:
+            self.__height = args[2]
+        if len(args) > 3:
+            self.__x = args[3]
+        if len(args) > 4:
+            self.__y = args[4]
+
+    def __str__(self):
+        """The method to print a string"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format\
+            (self.id, self.__x, self.__y, self.__width, self.__height)
 
     @property
     def width(self):
@@ -57,7 +78,7 @@ class Rectangle(Base):
         self.__width = value
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif (value <=0):
+        elif (value <= 0):
             raise ValueError("width must be > 0")
 
     @property
