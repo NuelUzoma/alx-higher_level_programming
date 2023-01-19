9907519abd9b311ebc73e507063437e63d5d729d
NuelUzoma <emcool17@gmail.com>
2023-01-19 02:16:26 +0100

    *args and **kwargs in the square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -138,6 +138,21 @@ class Square(Rectangle):
         """The method to print a string"""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
 
+    def update(self, *args, **kwargs):
+        """The method to print arguments"""
+        if len(args) > 0:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
     @property
     def size(self):
         """The getter for size"""
