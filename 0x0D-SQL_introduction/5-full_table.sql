4894379eb8c8039d1eeec90e445d03fd1ba2dd4a
NuelUzoma <emcool17@gmail.com>
2023-02-17 04:27:07 +0100

    Code Fix

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,4 @@
 -- Write a script that prints the full description of the table first_table
-DESC first_table;
+SELECT column_name, data_type, character_maximum_length, is_nullable, column_default
+FROM information_schema.columns
+WHERE table_name = 'first_table';
