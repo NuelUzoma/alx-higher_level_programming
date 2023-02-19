c29ee8ff5c73039e653792d5003d109bb880389b
NuelUzoma <emcool17@gmail.com>
2023-02-19 03:12:10 +0100

    Code Fix: Task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 -- the table cities (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS cities (
     `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `state_id` INT NOT NULL,
     `name` VARCHAR(256) NOT NULL,
