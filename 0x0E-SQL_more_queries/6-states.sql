e3465b8092aaeef9cbd975c19e7acde30d80aaa7
NuelUzoma <emcool17@gmail.com>
2023-02-19 03:09:17 +0100

    Code Fix: Task 6 and 7

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -5,7 +5,7 @@
 -- name VARCHAR(256) can’t be null
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE DATABASE IF NOT EXISTS states (
+CREATE TABLE IF NOT EXISTS states (
     `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(256) NOT NULL
 );
