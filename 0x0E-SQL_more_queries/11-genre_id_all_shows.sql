a1da0be682cc65ecf7f5dd522da90135b1e99d97
NuelUzoma <emcool17@gmail.com>
2023-02-22 00:50:09 +0100

    Code Fix: Task 11

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -3,4 +3,8 @@
 --Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- Results must be sorted in ascending order
 -- by tv_shows.title and tv_show_genres.genre_id
-SELECT tv_shows.title, COALESCE(tv_show_genres.genre_id, 'NULL') AS genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, COALESCE(tv_show_genres.genre_id, NULL) AS genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
