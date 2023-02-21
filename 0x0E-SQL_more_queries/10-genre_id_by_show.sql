52262448c16b3b31b7e9e27283761dc1cae8925d
NuelUzoma <emcool17@gmail.com>
2023-02-21 19:23:42 +0100

    Code Fix: Task 9 and Task 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,4 +2,4 @@
 -- to your MySQL server: download
 -- Write a script that lists all shows contained in hbtn_0d_tvshows
 -- that have at least one genre linked.
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres ORDER BY tv_shows.title ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- Write a script that lists all cities contained in the database hbtn_0d_usa.
-SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;
