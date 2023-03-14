011a0ac7abd60bcd0b134ef7f14a71865bd78f93
NuelUzoma <emcool17@gmail.com>
2023-03-14 01:38:35 +0100

    Task 4

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
 const arg = process.argv[2];
 
 if (!isNaN(arg)) {
-    console.log(`My number: ${parseInt(arg, 10)}`);
+  console.log(`My number: ${parseInt(arg, 10)}`);
 } else {
-    console.log("Not a number");
+  console.log('Not a number');
 }
