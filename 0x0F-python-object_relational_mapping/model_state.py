def2c95478048be21659b77cb2984805e5819b3e
NuelUzoma <emcool17@gmail.com>
2023-03-18 17:54:39 +0100

    SQLAlchemy First Model

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Write a python file that contains the class definition of a State
+        and an instance Base = declarative_base():
+        State class:
+        inherits from Base Tips
+        links to the MySQL table states
+        class attribute id that represents a column of an auto-generated,
+        unique integer, can’t be null and is a primary key
+        class attribute name that represents a column of a string
+        with maximum 128 characters and can’t be null"""
+
+
+from sqlalchemy import create_engine
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+import sys
+
+
+USR = sys.argv[1]
+PWD = sys.argv[2]
+DB = sys.argv[3]
+engine = create_engine('mysql://{}:{}@localhost:3306/{}'.format(USR, PWD, DB))
+Base = declarative_base()
+
+
+class State(Base):
+    """State Class that inherits from base"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+    def __init__(self, id, name):
+        """Intialization of the class from Base"""
+        self.id = id
+        self.name = name
+
+
+Base.metadata.create_all(engine)
