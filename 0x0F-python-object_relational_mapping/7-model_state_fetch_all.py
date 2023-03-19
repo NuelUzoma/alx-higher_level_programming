92ec3deb05a3a5efc4421a4e1a51e5fdf8ba8ea4
NuelUzoma <emcool17@gmail.com>
2023-03-19 01:36:58 +0100

    Fixed Typo

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python3
 """Write a script that lists all State objects from the database hbtn_0e_6_usa
-        Your script should take 3 arguments: mysql username,
-        mysql password and database name
-        You must use the module SQLAlchemy
-        You must import State and Base from model_state -
-        from model_state import Base, State
-        Your script should connect to a MySQL server running on
-        localhost at port 3306
-        Results must be sorted in ascending order by states.id"""
-
-
-import model_state
+Your script should take 3 arguments: mysql username,
+mysql password and database name
+You must use the module SQLAlchemy
+You must import State and Base from model_state -
 from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
-
-
-Session = sessionmaker(bind=model_state.engine)
-session = Session()
-
-
-def model_state_fetch_all():
-    """A function to query using SQLAlchemy module to send queries"""
-
-    for instance in session.query(State).order_by(State.id):
-        print("{}: {}".format(instance.id, instance.name))
+Your script should connect to a MySQL server running on
+localhost at port 3306
+Results must be sorted in ascending order by states.id
+"""
 
 
 if __name__ == "__main__":
-    model_state_fetch_all()
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
+    import sys
+    
+    USR = sys.argv[1]
+    PWD = sys.argv[2]
+    DB = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(USR, PWD, DB), echo=True)
+    engine.connect()
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        print("{}: {}".format(instance.id, instance.name))
+    session.commit()
+    session.close()
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,40 +1,42 @@
 #!/usr/bin/python3
 """Write a python file that contains the class definition of a State
-        and an instance Base = declarative_base():
-        State class:
-        inherits from Base Tips
-        links to the MySQL table states
-        class attribute id that represents a column of an auto-generated,
-        unique integer, cannot be null and is a primary key
-        class attribute name that represents a column of a string
-        with maximum 128 characters and cannot be null"""
+and an instance Base = declarative_base():
+State class:
+inherits from Base Tips
+links to the MySQL table states
+class attribute id that represents a column of an auto-generated,
+unique integer, cannot be null and is a primary key
+class attribute name that represents a column of a string
+with maximum 128 characters and cannot be null
+"""
 
 
-from sqlalchemy import create_engine, Column, String, Integer
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-import sys
-
-
-USR = sys.argv[1]
-PWD = sys.argv[2]
-DB = sys.argv[3]
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+if __name__ == "_main__":
+     from sqlalchemy import create_engine, Column, String, Integer
+     from sqlalchemy.orm import sessionmaker
+     from sqlalchemy.ext.declarative import declarative_base
+     import sys
+     
+     
+     USR = sys.argv[1]
+     PWD = sys.argv[2]
+     DB = sys.argv[3]
+     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                        .format(USR, PWD, DB), echo=True)
-Base = declarative_base()
+     engine.connect()
+     Base = declarative_base()
+     
+     
+     class State(Base):
+         """State Class that inherits from base linking to the states table"""
+         __tablename__ = 'State'
 
+         id = Column(Integer, primary_key=True, nullable=False)
+         name = Column(String(128), nullable=False)
 
-class State(Base):
-    """State Class that inherits from base linking to the states table"""
-    __tablename__ = 'State'
+         def __init__(self, id, name):
+              """Intialization of the class from Base"""
+              self.id = id
+              self.name = name
 
-    id = Column(Integer, primary_key=True, nullable=False)
-    name = Column(String(128), nullable=False)
-
-    def __init__(self, id, name):
-        """Intialization of the class from Base"""
-        self.id = id
-        self.name = name
-
-
-Base.metadata.create_all(engine)
+     Base.metadata.create_all(engine)
