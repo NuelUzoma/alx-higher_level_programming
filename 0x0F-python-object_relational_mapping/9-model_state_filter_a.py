871f84b4de3495491cb20ddab5c31eb782e6fb72
NuelUzoma <emcool17@gmail.com>
2023-03-19 04:14:58 +0100

    Code Commit: Task 9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Write a script that prints the
-first State object from the database hbtn_0e_6_usa
+"""Write a script that lists all State objects that contain the letter a
+from the database hbtn_0e_6_usa
 Your script should take 3 arguments: mysql username,
 mysql password and database name
 You must use the module SQLAlchemy
@@ -8,8 +8,7 @@ You must import State and Base from model_state -
 from model_state import Base, State
 Your script should connect to a MySQL server running on
 localhost at port 3306
-You are not allowed to fetch all states from the database
-before printing the first one
+Results must be sorted in ascending order by states.id
 """
 if __name__ == "__main__":
     import sys
@@ -21,15 +20,13 @@ if __name__ == "__main__":
     PWD = sys.argv[2]
     DB = sys.argv[3]
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(USR, PWD, DB), echo=True)
+                            .format(USR, PWD, DB), echo=True)
     engine.connect()
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    first_state = session.query(State).filter_by(State.id=1).order_by(State.id).first()
-    if first_state:
-        print("{}: {}".format(first_state.id, first_state.name))
-    else:
-        print("Nothing")
+    lists = session.query(State).filter(State.name.like('%a%')).order_by(State.id)
+    for list in lists:
+        print("{}: {}".format(list.id, list.name))
     session.commit()
     session.close()
