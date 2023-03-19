eccac1bfff3decafc7771bd69a492df341484c30
NuelUzoma <emcool17@gmail.com>
2023-03-19 04:18:51 +0100

    Fixed Typos for Task 9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -20,12 +20,13 @@ if __name__ == "__main__":
     PWD = sys.argv[2]
     DB = sys.argv[3]
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                            .format(USR, PWD, DB), echo=True)
+                           .format(USR, PWD, DB), echo=True)
     engine.connect()
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    lists = session.query(State).filter(State.name.like('%a%')).order_by(State.id)
+    lists = session.query(State).filter(State.name.like('%a%')
+                                        ).order_by(State.id)
     for list in lists:
         print("{}: {}".format(list.id, list.name))
     session.commit()
