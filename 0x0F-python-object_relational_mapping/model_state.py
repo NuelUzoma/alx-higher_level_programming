64adc8a6ea5b93d6f95d14da6ba5c28783062686
NuelUzoma <emcool17@gmail.com>
2023-03-19 02:06:17 +0100

    Task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,24 +11,25 @@ with maximum 128 characters and cannot be null
 """
 
 
-if __name__ == "__main__":
-    from sqlalchemy import create_engine
-    from sqlalchemy import Column, String, Integer
-    from sqlalchemy.ext.declarative import declarative_base
-    import sys
+from sqlalchemy import create_engine
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+import sys
 
-    USR = sys.argv[1]
-    PWD = sys.argv[2]
-    DB = sys.argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(USR, PWD, DB), echo=True)
-    Base = declarative_base()
-    Base.metadata.create_all(engine)
+USR = sys.argv[1]
+PWD = sys.argv[2]
+DB = sys.argv[3]
+engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                       .format(USR, PWD, DB))
+Base = declarative_base()
 
-    class State(Base):
-        """State Class that inherits from base linking to the states table"""
-        __tablename__ = 'states'
 
-        id = Column(Integer, primary_key=True,
-                    autoincrement=True, nullable=False)
-        name = Column(String(128), nullable=False)
+class State(Base):
+    """State Class that inherits from base linking to the states table"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True,
+                autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+
+Base.metadata.create_all(engine)
