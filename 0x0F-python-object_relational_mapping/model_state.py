993faa34ceabbc1a2a60c6f9847d32f8f1b76edd
NuelUzoma <emcool17@gmail.com>
2023-03-20 02:15:54 +0100

    Task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -13,19 +13,19 @@ before calling Base.metadata.create_all(engine)
 """
 
 
-if __name__ == "__main__":
-    from sqlalchemy import Column, String, Integer
-    from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
 
-    Base = declarative_base()
+Base = declarative_base()
 
-    class State(Base):
-        """State Class that inherits from base linking to the states table
-        class attribute id that represents a column of an auto-generated,
-        unique integer, can't be null and is a primary key
-        class attribute name that represents a column of a string
-        with maximum 128 characters and can't be null"""
-        __tablename__ = 'states'
-        id = Column(Integer, primary_key=True,
-                    autoincrement=True, nullable=False)
-        name = Column(String(128), nullable=False)
+
+class State(Base):
+    """State Class that inherits from base linking to the states table
+    class attribute id that represents a column of an auto-generated,
+    unique integer, can't be null and is a primary key
+    class attribute name that represents a column of a string
+    with maximum 128 characters and can't be null"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True,
+                autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
