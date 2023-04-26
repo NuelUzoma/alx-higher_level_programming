137eb52a9cf3a6980a6d7e543fc7a0ee7216008b
NuelUzoma <emcool17@gmail.com>
2023-04-26 18:24:47 +0100

    Return the number of times of a movie using Star Wars API

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,8 +4,8 @@ const request = require('request');
 const URL = process.argv[2];
 const filmId = '18';
 
-request(URL, function (error, body) {
-  if (!error && request.statusCode === 200) {
+request(URL, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
     const filmsData = JSON.parse(body).results;
     const movies = filmsData.filter(film => {
       return film.characters.indexOf(`https://swapi-api.alx-tools.com/api/people/${filmId}/`) !== -1;
