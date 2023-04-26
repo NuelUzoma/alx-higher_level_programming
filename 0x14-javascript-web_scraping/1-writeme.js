4ad26e67152f18afb3d2d38d3473290415a71ab9
NuelUzoma <emcool17@gmail.com>
2023-04-26 04:26:10 +0100

    Write to a file using fs module

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filename = process.argv[2];
+const content = process.argv[3];
+fs.writeFile(filename, content, function (err) {
+  if (err) {
+    console.error(err);
+  }
+});
